8d331b4744030189bc997063c472ceb3403fd231
MHImani <149239026+MMousavi0026@users.noreply.github.com>
2023-11-14 21:33:37 +0330

    0

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -7,34 +7,29 @@ import {faBasketShopping, faHeart, faMagnifyingGlass, faPhoneVolume, faUser} fro
 const SiteHeader = (props) => {
     return (
         <div className={Styles.headerWrapper}>
-            <div className={Styles.headerItemsWrapper}>
-                <div className={Styles.headerItems}>
-                    <img className={Styles.logo} alt='logo' src={require('../assets/img/logo-sm.png')}/>
-                    <div className={Styles.searchBox}>
-                        <input className={Styles.input} type={"text"} placeholder={'دنبال چه محصولی هستید؟'}/>
-                        <button className={Styles.searchIcon+" "+Styles.button}>
-                            <FontAwesomeIcon className={Styles.icon} icon={faMagnifyingGlass} />
-                        </button>
-                    </div>
-                    <div className={Styles.buttonsWrapper}>
-                        <button className={Styles.call}>
-                            <FontAwesomeIcon className={Styles.icon+" "+Styles.callIcon} icon={faPhoneVolume} />
-                            <div className={Styles.phone}>
-                                <p className={Styles.phoneNumber}>شماره تماس</p>
-                                <p>0922334455</p>
-                            </div>
-                        </button>
-                        <button className={Styles.button}>
-                            <FontAwesomeIcon className={Styles.icon} icon={faBasketShopping} />
-                        </button>
-                        <button className={Styles.button}>
-                            <FontAwesomeIcon className={Styles.icon} icon={faHeart} />
-                        </button>
-                        <button className={Styles.button}>
-                            <FontAwesomeIcon className={Styles.icon} icon={faUser} />
-                        </button>
-                    </div>
+            <div className={Styles.header1}>
+                <img className={Styles.logo} alt='logo' src={require('../assets/img/logo-sm.png')}/>
+                <div className={Styles.searchBox}>
+                    <input className={Styles.input} type={"text"} placeholder={'دنبال چه محصولی هستید؟'}/>
+                    <button className={Styles.searchIcon+" "+Styles.button}>
+                        <FontAwesomeIcon className={Styles.icon} icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+                <a className={Styles.call}>
+                    <FontAwesomeIcon className={Styles.icon+" "+Styles.callIcon} icon={faPhoneVolume} />
+                    <span style={{fontSize:"14px"}}>شماره تماس</span>
+                    <span style={{fontSize:"14px"}}>0922334455</span>
+                </a>
+                <a className={Styles.store}>
+                    <FontAwesomeIcon className={Styles.icon} icon={faBasketShopping} />
+                </a>
+                <a className={Styles.love}>
+                    <FontAwesomeIcon className={Styles.icon} icon={faHeart} />
+                </a>
+                <a className={Styles.profile}>
+                    <FontAwesomeIcon className={Styles.icon} icon={faUser} />
+                </a>
+
             </div>
             <div className={Styles.navMenu}>
 
